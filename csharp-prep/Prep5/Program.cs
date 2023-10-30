b1791cf76d1cf146038e51af0dd369968fdf096e
Luis Rivera <LuisRaulRivera24@gmail.com>
2023-10-30 18:39:41 -0400

    Location of the creation of the functions was moved outside of the main function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,37 +9,36 @@ class Program
         int number = PromptUserNumber ();
         int numSquared = SquareNumber (number);
         DisplayResult (name, numSquared);
+    }
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the Program!");
+    }
 
-        static void DisplayWelcome ()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Welcome to the Program!");
-        }
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
 
-        static string PromptUserName ()
-        {
-            Console.Write("Please enter your name: ");
-            string userName = Console.ReadLine();
-            return userName;
-        }
+    static int PromptUserNumber ()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
 
-        static int PromptUserNumber ()
-        {
-            Console.Write("Please enter your favorite number: ");
-            int favoriteNumber = int.Parse(Console.ReadLine());
-            return favoriteNumber;
-        }
+    static int SquareNumber (int num)
+    {
+        int squaredNum = num * num;
+        return squaredNum;
+    }
 
-        static int SquareNumber (int num)
-        {
-            int squaredNum = num * num;
-            return squaredNum;
-        }
-
-        static void DisplayResult (string name, int squaredNum)
-        {
-            Console.WriteLine($"{name}, the square of your number is {squaredNum}");
-            Console.WriteLine();
-        }
+    static void DisplayResult (string name, int squaredNum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNum}");
+        Console.WriteLine();
     }
 }
