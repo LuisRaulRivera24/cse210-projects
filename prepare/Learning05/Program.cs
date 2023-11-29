1bb570b046144708dec16c63a57525be320d13a8
Luis Rivera <LuisRaulRivera24@gmail.com>
2023-11-28 23:33:28 -0500

    finished Polymorphism Learning Activity assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine();
+        Square square = new Square("blue", 6);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+        Console.WriteLine();
+
+        Rectangle rectangle = new Rectangle("red", 8, 5);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+        Console.WriteLine();
+
+        Circle circle = new Circle("orange", 3);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+        Console.WriteLine();
+
+        List<Shape> shapesList = new List<Shape>();
+        shapesList.Add(square);
+        shapesList.Add(rectangle);
+        shapesList.Add(circle);
+
+        foreach (Shape shape in shapesList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,16 @@
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        double squareArea = _side * _side;
+        return squareArea;
+    }
+
+}
