e5d7dac4ce2e17bbd62eede738c65a952d4652da
Luis Rivera <LuisRaulRivera24@gmail.com>
2023-12-13 00:25:45 -0500

    small change made to foundation 4 assigment

--- final/Foundation4/ActivityManager.cs
+++ final/Foundation4/ActivityManager.cs
@@ -5,7 +5,7 @@ public class ActivityManager
     public ActivityManager()
     {
         _activityList = new List<Activity>();
-
+    
         Running running = new Running("03 Nov 2022", 30, 3.0);
         Cycling cycling = new Cycling("05 Mar 2023", 50, 8);
         Swimming swimming = new Swimming("14 Jul 2023", 43, 30);
